fe51bc64ca40cab719d43262a425cdaffddc4a9c
Ap0l1js <ap0l1js@ghost.ghost.com>
2023-05-03 20:29:52 +0100

    improvements: add useForm and zod to form

--- src/containers/task_container/creator_form/index.tsx
+++ src/containers/task_container/creator_form/index.tsx
@@ -1,14 +1,40 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { PlusCircle } from '@phosphor-icons/react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { CreatorFormContainer } from './styles';
 
+const creatorFormSchema = z.object({
+  task: z.string(),
+});
+
+type CreatorFormInputs = z.infer<typeof creatorFormSchema>;
+
 export function CreatorForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<CreatorFormInputs>({
+    resolver: zodResolver(creatorFormSchema),
+  });
+
+  async function handleCreateTask(data: CreatorFormInputs) {
+    console.log('dataaa: ', data);
+  }
+
   return (
-    <CreatorFormContainer>
+    <CreatorFormContainer onSubmit={handleSubmit(handleCreateTask)}>
       <input
         type="text"
+        required
         placeholder="Adicione uma nova tarefa"
+        {...register('task')}
       />
-      <button type="submit">
+      <button
+        type="submit"
+        disabled={isSubmitting}
+      >
         Criar
         <PlusCircle
           weight="bold"
